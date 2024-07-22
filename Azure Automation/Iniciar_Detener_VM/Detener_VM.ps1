b04f8fbeece9d2cb32ae1fd00690aaaac554dd4d
Infra Script Automate <InfraScriptAutomate@gmail.com>
2024-07-22 18:34:37 -0500

    Add files via upload

---
+++ Azure Automation/Iniciar_Detener_VM/Detener_VM.ps1
@@ -0,0 +1,13 @@
+﻿# Importar módulos necesarios
+Import-Module Az.Compute
+Import-Module Az.Resources
+
+# Variables
+$resourceGroupName = "yourResourceGroupName"
+$vmName = "yourVMName"
+
+# Iniciar la máquina virtual
+Stop-AzVM -ResourceGroupName $resourceGroupName -Name $vmName
+
+# Mensaje de salida
+Write-Output "La máquina virtual $vmName en el grupo de recursos $resourceGroupName ha sido iniciada."
