a0e1870effadd8f8bb9c46f5d547d9f442a87949
Infra Script Automate <InfraScriptAutomate@gmail.com>
2024-07-28 20:12:00 -0500

    Add files via upload

---
+++ Azure PowerShell/Login-Az-PowerShell.ps1
@@ -0,0 +1,14 @@
+﻿# Paso 1.- Instalar módulos Az
+Install-Module -Name Az -AllowClobber -Force 
+
+# Paso 2.- Permitir ejecución de scripts
+Set-ExecutionPolicy RemoteSigned 
+
+# Autenticarse en Azure 
+Connect-AzAccount
+
+# Verificar conexión 
+Get-AzSubscription 
+
+#Conectarte a una suscripción en específica
+Connect-AzAccount | Select-AzSubscription -SubscriptionId "ID-De-Tu-Suscripcion"
