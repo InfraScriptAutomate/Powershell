0c108acd35b0c49097bf2bf736c1bfbf598e0ee2
Infra Script Automate <InfraScriptAutomate@gmail.com>
2024-07-31 21:50:17 -0500

    Add files via upload

---
+++ Extender Vigencia PowerShell/CreaciónSecretos_MasAños.ps1
@@ -0,0 +1,21 @@
+﻿# Conexión Entra ID
+Connect-AzureAD
+
+#Variables
+$startDate = Get-Date
+#Colocar el # de años que caducarpa el secreto
+$endDate = $startDate.AddYears(999)
+
+#Agregar Secreto
+$aadappsecret = New-AzureADApplicationPasswordCredential -ObjectId "Object ID App Entra ID" -CustomKeyIdentifier "NombreSecreto" -StartDate $startDate -EndDate $enddate
+
+#Muestra Secreto
+$aadappsecret
+
+
+
+
+
+
+
+
