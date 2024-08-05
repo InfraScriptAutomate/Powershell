d3d7bbf004d0dd0bf164ecda43f686a451af9cdc
Infra Script Automate <InfraScriptAutomate@gmail.com>
2024-08-04 22:08:28 -0500

    Add files via upload

---
+++ Habilitar Autoarchivado Buzones 365/Autoarchive-DomainGroup.ps1
@@ -0,0 +1,5 @@
+﻿$Users=Get-ADGroupMember -Identity "Grupo de Usuario para habilitar autoarchivado" | select -expand samaccountname
+foreach ($LINE in $Users)  
+{
+Enable-RemoteMailbox -Identity $LINE -Archive
+}
---
+++ Habilitar Autoarchivado Buzones 365/Autoarchive-WithPath.ps1
@@ -0,0 +1,6 @@
+﻿$FILE = Get-Content "C:/Path del archivo/test.txt"
+foreach ($LINE in $FILE)  
+{
+Enable-RemoteMailbox -Identity $LINE -Archive
+}
+
